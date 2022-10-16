0ed0167778ca8f6fb2dd1bc9c5539af5d10bf655
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-16 14:18:45 +0200

    Step 7 - Frontend up and running

---
+++ client/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const element = document.getElementById("app");
+const root = createRoot(element);
+root.render(<h1>Greetings everyone!</h1>)
