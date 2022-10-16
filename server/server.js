f87620bdec237d393c5880ed5c0b4c4bb80f2929
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-16 14:57:11 +0200

    Step 12 - If I get a "GET" and it's not for the "/api/" path, just return index

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 
 app.use(express.static("../client/dist/"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 app.get("/api/login", (req, res) => {
   res.json({
