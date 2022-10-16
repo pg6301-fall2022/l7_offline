13e79d019e022cb011320064ed513bfc91bdc1dc
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-16 14:53:41 +0200

    Step 11 - Simple GET endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.use(express.static("../client/dist/"));
 
+app.get("/api/login", (req, res) => {
+  res.json({
+    username: "admin",
+    fullname: "Testsson",
+  });
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });
